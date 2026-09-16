ef7fd5994048bfe48e84c4e89d9d55b79eec6c17
AGr-777 <agrajakku777a@gmail.com>
2026-09-16 17:00:15 +0530

    Completed Day 28 User Detail Practice

---
+++ day28.js
@@ -0,0 +1,11 @@
+let name = "Agraj";
+let age = 25;
+let place = "Cherthala";
+
+console.log(name);
+console.log(age);
+console.log(place);
+
+console.log("My name is " + name);
+console.log("I am" + age + "years old");
+console.log("I am from " + place);
