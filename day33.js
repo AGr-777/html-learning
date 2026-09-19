89eaeb14415925d45cfbff7e94ab72b06fdcee1c
AGr-777 <agrajakku777a@gmail.com>
2026-09-19 16:12:59 +0530

    Completed day33 project Return keyword

---
+++ day33.js
@@ -0,0 +1,19 @@
+function multiply(a, b) {
+    return a * b;
+}
+let result = multiply(10, 5);
+console.log(result);
+
+
+function getName() {
+    return "Agraj";
+}
+let name = getName();
+console.log(name);
+
+
+function add(a, b) {
+    return a + b;
+}
+let total = add(10, 5);
+console.log(result);
