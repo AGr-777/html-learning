26f1a41cafe4246a87a0a1e1db93fd57be261382
AGr-777 <agrajakku777a@gmail.com>
2026-09-19 16:36:06 +0530

    Completed Day34 project Button Click Event

---
+++ day34.js
@@ -0,0 +1,12 @@
+function showMessage() {
+    alert("Hello Agraj");
+}
+
+function showMessage() {
+    alert("Welcome to Mern Stack");
+}
+
+
+function showMessage() {
+    console.log("Button Clicked");
+}
