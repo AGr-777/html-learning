cc6129e5d8e5e2884b8736f5cac8c7b5833dc4fc
AGr-777 <agrajakku777a@gmail.com>
2026-09-24 14:32:37 +0530

    Completed Day 35 project DOM Manipulation (Text Change)

---
+++ day35.js
@@ -0,0 +1,8 @@
+function changeText() {
+    document.getElementById("title").innerHTML = "Agraj is learning JavaScript";
+}
+
+
+function changeText() {
+    document.getElementById("message").innerHTML = "Button Clicked";
+}
